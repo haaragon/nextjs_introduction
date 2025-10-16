b1a0fa93f5ed9ccea77f8198df75385a93caec88
haaragon <hernan.aragon23@gmail.com>
2025-10-15 20:20:47 -0600

    feat: basic nextjs introduction

---
+++ app/page.js
@@ -0,0 +1,22 @@
+import LikeButton from "./like-button";
+
+function Header({ title }) {
+  return <div>{title ? title : "Default title"}</div>;
+}
+
+export default function HomePage() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+
+  return (
+    <>
+      <Header title="React" />
+      <Header title="A new title" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
+    </>
+  );
+}
